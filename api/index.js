05618178483392cf1303d0101cc5e383e89a0e39
stephwalt04 <kolawoledolapo115@gmail.com>
2024-09-21 08:52:57 -0700

    json response

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.json("Express on Vercel"));
 
 app.get("/api", (req, res) => {
   res.json({ name: "John Doe" });
